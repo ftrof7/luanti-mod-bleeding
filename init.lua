a79f321c4ddd1f68db8dc4f5da5932cef5718713
ftrof <112196266+ftrof7@users.noreply.github.com>
2025-02-08 20:56:21 +0200

    uploading mod

---
+++ init.lua
@@ -0,0 +1,121 @@
+local bloods = {}
+
+local effects = {
+    {text = "bleeding_screen1.png", speed = 0.9},
+    {text = "bleeding_screen2.png", speed = 0.75},
+    {text = "bleeding_screen3.png", speed = 0.55},
+    {text = "bleeding_screen4.png", speed = 0.2},
+}
+
+local spawn_blood_particles = function(pos, amount)
+    minetest.add_particlespawner({
+        amount = amount,
+        time = 0.5,
+        minpos = vector.subtract(pos, {x=0.2, y=0.2, z=0.2}),
+        maxpos = vector.add(pos, {x=0.2, y=0.2, z=0.2}),
+        minvel = {x=-2, y=3, z=-2},
+        maxvel = {x=2, y=5, z=2},
+        minacc = {x=0, y=-9.81, z=0},
+        maxacc = {x=0, y=-9.81, z=0},
+        minsize = 1,
+        maxsize = 2,
+        texture = "bleeding_particle.png",
+        collisiondetection = true,
+        collision_removal = true,
+        vertical = false,
+    })
+end
+
+local timer = 0
+minetest.register_globalstep(function(dtime)
+    timer = timer + dtime
+    if timer >= 1 then
+        timer = 0
+        for key, value in pairs(bloods) do
+            local player = minetest.get_player_by_name(key)
+            if value.bleeding then
+                player:set_hp(player:get_hp() - 1)
+                spawn_blood_particles(player:get_pos(), 10)
+                if bloods[key].bleeding <= 1 then
+                    bloods[key].bleeding = nil
+                else
+                    bloods[key].bleeding = bloods[key].bleeding - 1
+                end
+            end
+        end
+    end
+end)
+
+minetest.register_on_player_hpchange(function(player, hp_change, reason)
+    local name = player:get_player_name()
+    if hp_change > 0 and bloods[name] then
+        player:hud_remove(bloods[name].hud)
+        bloods[name] = nil
+    end
+ 
+    if hp_change >= 0 or math.abs(hp_change) > player:get_hp() then return end
+
+    local name = player:get_player_name()
+    local damage = math.abs(hp_change)
+    local pos = player:get_pos()
+    local stage = 0
+    local speed_before = player:get_physics_override().speed
+
+    if damage < 4 then
+        stage = 1
+    elseif damage < 9 then
+        stage = 2
+        spawn_blood_particles(pos, 10)
+    elseif damage < 14 then
+        stage = 3
+        spawn_blood_particles(pos, 23)
+    else
+        stage = 4
+        spawn_blood_particles(pos, 32)
+    end
+    if bloods[name] then
+       return
+    end
+    bloods[name] = {}
+    bloods[name].hud = player:hud_add({
+		hud_elem_type = "image",
+		position = {x = 0.5, y = 0.5},
+		scale = {x = -100,y = -100},
+		text = effects[stage].text,
+		alignment = {x = 0, y = 0},
+		offset = {x = 0, y = 0}
+	})
+
+    for i = stage, 1, -1 do
+        minetest.after(2 * (stage - i + 1), function()
+            if bloods[name] then
+                player:hud_change(bloods[name].hud, "text", effects[i].text)
+                if i > 1 then
+                    bloods[name].bleeding = i
+                end
+            end
+        end)
+    end
+
+    minetest.after(2*stage+1, function()
+        if bloods[name] then
+            player:hud_remove(bloods[name].hud)
+            bloods[name] = nil 
+        end
+    end)
+end)
+
+minetest.register_on_respawnplayer(function(player)
+    local name = player:get_player_name()
+    if bloods[name] then
+        player:hud_remove(bloods[name].hud)
+        bloods[name] = nil
+    end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+    local name = player:get_player_name()
+    if bloods[name] then
+        bloods[name] = nil
+    end
+end)
